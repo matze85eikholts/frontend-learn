8170893a279ab6ef0723198781989d00c6c46775
Matvey Eikholts <matze85eikholts@gmail.com>
2023-12-04 04:40:31 +0300

    TicTacToe changed

--- 01-react-basics-project/tictactoe-app/src/TicTacToe/TicTacToe.jsx
+++ 01-react-basics-project/tictactoe-app/src/TicTacToe/TicTacToe.jsx
@@ -1,34 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TicTacToe.css';
 
 const TicTacToe = () => {
-  const Cell = () => {
-    return <td onClick={() => handleClick()}>-</td>;
+  const [turn, setTurn] = useState('x');
+  const [cells, setCells] = useState(Array(9).fill(''));
+  const Cell = ({ num }) => {
+    return <td onClick={() => handleClick(num)}>-</td>;
   };
 
-  const handleClick = () => {
-    alert('Ячейка была кликнута');
+  const handleClick = (num) => {
+    //alert(num);
+    let squars = [...cells];
+    if (turn === 'x') {
+      squars[num] = 'x';
+      setTurn('0');
+    } else {
+      squars[num] = '0';
+      setTurn('x');
+    }
+    setCells(squars);
   };
 
   return (
     <div>
       <h1>Игра в крестики и нолики!</h1>
       <table>
+        Turn: {turn}
         <tbody>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={0} />
+            <Cell num={1} />
+            <Cell num={2} />
           </tr>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={3} />
+            <Cell num={4} />
+            <Cell num={5} />
           </tr>
           <tr>
-            <Cell />
-            <Cell />
-            <Cell />
+            <Cell num={6} />
+            <Cell num={7} />
+            <Cell num={8} />
           </tr>
         </tbody>
       </table>
